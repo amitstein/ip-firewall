c44fbd3ee5205b5f26a85c0ea7ee628fbc8803ec
amitstein <amitstein@gmail.com>
2024-02-18 19:26:45 -0300

    Move retrieve data method

--- src/main/java/org/amitstein/IPService.java
+++ src/main/java/org/amitstein/IPService.java
@@ -1,10 +1,16 @@
 package org.amitstein;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.quarkus.scheduler.Scheduled;
@@ -34,6 +40,36 @@ public class IPService {
         System.out.println(stopTime - startTime);
     }
 
+    public void retrieveData() throws IOException, InterruptedException {
+        String owner = "stamparm";
+        String repo = "ipsum";
+        String filePath = "master/ipsum.txt";
+
+        // Construct the GitHub API URL to retrieve the file
+        String apiUrl = String.format("https://raw.githubusercontent.com/%s/%s/%s", owner, repo, filePath);
+
+        // Create an HTTP client
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Create a GET request to the GitHub API
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .build();
+
+        // Send the request and handle the response
+        HttpResponse<byte[]> downloadResponse = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
+        if (downloadResponse.statusCode() == 200) {
+            // If the download request was successful, write the content of the file to a local file
+            byte[] fileContent = downloadResponse.body();
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            outputStream.write(fileContent);
+            outputStream.close();
+            System.out.println("File downloaded successfully.");
+        } else {
+            System.out.println("Failed to download file. Status code: " + downloadResponse.statusCode());
+        }
+    }
+    
     private void readFileFromResources() {
         try (InputStreamReader inputStreamReader = new FileReader(fileName);
              BufferedReader reader = new BufferedReader(inputStreamReader)) {
--- src/main/java/org/amitstein/Main.java
+++ src/main/java/org/amitstein/Main.java
@@ -7,9 +7,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.inject.Inject;
 
 @QuarkusMain
 public class Main {
@@ -20,10 +23,13 @@ public class Main {
 
     public static class IPFirewallApp implements QuarkusApplication {
 
+        @Inject
+        IPService ipService;
+
         @Override
         public int run(String... args) throws Exception {
             if (elementExists(args, "fetch")) {
-                retrieveFile();
+                ipService.retrieveData();
             }
             else {
                 Quarkus.waitForExit();
@@ -41,35 +47,4 @@ public class Main {
         }
         return false;
     }
-
-    public static void retrieveFile() throws IOException, InterruptedException {
-        String owner = "stamparm";
-        String repo = "ipsum";
-        String filePath = "master/ipsum.txt";
-
-        // Construct the GitHub API URL to retrieve the file
-        String apiUrl = String.format("https://raw.githubusercontent.com/%s/%s/%s", owner, repo, filePath);
-
-        // Create an HTTP client
-        HttpClient client = HttpClient.newHttpClient();
-
-        // Create a GET request to the GitHub API
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl))
-                .build();
-
-        // Send the request and handle the response
-        HttpResponse<byte[]> downloadResponse = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
-        if (downloadResponse.statusCode() == 200) {
-            // If the download request was successful, write the content of the file to a local file
-            byte[] fileContent = downloadResponse.body();
-            FileOutputStream outputStream = new FileOutputStream("/Users/amit/ipsum.txt");
-            outputStream.write(fileContent);
-            outputStream.close();
-            System.out.println("File downloaded successfully.");
-        } else {
-            System.out.println("Failed to download file. Status code: " + downloadResponse.statusCode());
-        }
-    }
-
 }
